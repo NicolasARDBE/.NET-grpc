d64c019acac1b46dcf9408dc710d0a3e1f8334f1
NicolasARDBE <nicolasardbe@gmail.com>
2024-11-04 19:09:06 -0500

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+channel.Dispose();
---
+++ GrpcServer/Services/GreeterService.cs
@@ -0,0 +1,15 @@
+using Grpc.Core;
+using GrpcServer;
+
+namespace GrpcServer.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = $"Hello, {request.Name}!"
+        });
+    }
+}
